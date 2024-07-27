f40018729d2ce4e6bea7591dfddc580f09e0375c
TechSec <88988482+TechSecWasTaken@users.noreply.github.com>
2024-07-27 06:30:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ messages = []
 old_prompt = ""
 hourLength = 3
 winner = None
+pingRoleID = 0
 
 @bot.event
 async def on_ready():
@@ -27,31 +28,46 @@ async def on_message(ctx):
 
 @tasks.loop(seconds=5.0)
 async def hold(ctx):
-    print("started")
     global thread_id
     global old_prompt
+    global pingRoleID
+
+    print("NEW GAME")
     
     blackCards = open('blackcards.txt').read().splitlines()
     prompt = random.choice(blackCards)
-
     while prompt == old_prompt:
         prompt = random.choice(blackCards)
 
-    msg = await ctx.send(prompt)
+    if pingRoleID > 0:
+        msg = await ctx.send(f"<@&{pingRoleID}>\n"+prompt)
+
     thread = await msg.create_thread(name="Game Responses")
     thread_id = thread.id
+    await thread.edit(
+        slowmode_delay = (21600)
+    )
 
     await asyncio.sleep(hourLength * 3600)
 
     global messages
     global winner
 
+    await thread.edit(
+        archived = True,
+        locked = True,
+        slowmode_delay = (21600)
+    )
+
     print("Timer ended")
     for msg in messages:
         if winner == None: 
             winner = msg
             print(winner)
-        
+
+        if bot.get_message(msg) == None:
+            return
+
         if bot.get_message(msg).reactions[0].count > bot.get_message(winner).reactions[0].count:
             print(winner)
             winner = msg
